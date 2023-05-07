342a08b016c9d05a1600b0a2ef562e953d237644
pkn <Lixka@mbp-de-chelsea.home>
2023-05-07 16:36:19 +0200

    cherche une solution de win des 4 balles d'affile sur papier

--- connect-four/app.js
+++ connect-four/app.js
@@ -39,23 +39,29 @@ function choiceCase(){
     console.log('create case')
     if (i % 2 == 0){
         const ballUserOne = document.createElement('div')
-        //const varIdCaseRed = this.getAttribut('index_ligne')
         ballUserOne.style.width = '20px'
         ballUserOne.style.height = '20px'
         ballUserOne.style.borderRadius = '10px'
         ballUserOne.style.backgroundColor = 'red'
+        const varIdCaseRed = this.getAttribute('index_ligne')
+        console.log(varIdCaseRed)
         this.appendChild(ballUserOne)
-        //arrayWinRed.push(varIdCaseRed)
+        arrayWinRed.push(varIdCaseRed)
+        //win()
+        console.log(arrayWinRed)
     } else {
         const ballUserTwo = document.createElement('div')
-        //const varIdCaseBlue = this.getAttribut('index_ligne')
         ballUserTwo.style.width = '20px'
         ballUserTwo.style.height = '20px'
         ballUserTwo.style.borderRadius = '10px'
-        ballUserTwo.style.backgroundColor = 'red'
         ballUserTwo.style.backgroundColor = 'blue'
+        const varIdCaseBlue = this.getAttribute('index_ligne')
         this.appendChild(ballUserTwo)
-        //arrayWinBlue.push(varIdCaseBlue)
+        arrayWinBlue.push(varIdCaseBlue)
+        if (arrayWinBlue.length >= 4){
+            win()
+        }
+        console.log(arrayWinRed)
     }
     i++
     this.removeEventListener('click', choiceCase)
@@ -69,10 +75,35 @@ function choiceCase(){
 
 le gris est une suite de 10 tableau de 10 case.
 On peut mettre un id de 0 a 9 pour chaque tableau 
+- Donc si dans le tableau de valeur, on retrouve 4 fois d'afillé le meme chiffre, c'est gagné.
+- Si on retrouve une suite de chiffre qui se suit, c'est gagné
+
+for (let i = 0; i < 4; i++){
+    if (array[i] == aaray[i+1]){
+        ok = 1
+        i++
+    }else {
+        ok = 0
+    }
+    if (ok == 1){
+        console.log('c'est gagne)
+    }
+}  ET IL FAUT VERIFIER SI LES CASES SE COLLET, AVEC UN MODULU TOUT LES 10 CASES
 
 */
 function win() {
-
+    let ok = 0
+    for (let i = -1; i < 4; ++i){
+        if ((arrayWinBlue[arrayWinBlue.length - i] == arrayWinBlue[i+1]) && (arrayHundred[i+10] == arrayHundred[i++])){
+            ok = 1
+            i++
+        }else {
+            ok = 0
+        }
+        if (ok == 1){
+            console.log("c'est gagne")
+        }
+    }   
 }
 
 
