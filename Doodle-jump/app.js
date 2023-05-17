6d005657d782c26e33706b453d3fea204ccc8dc8
pkn <Lixka@mbp-de-chelsea.home>
2023-05-17 16:41:32 +0200

    ajout de commentaire explicatif

--- Doodle-jump/app.js
+++ Doodle-jump/app.js
@@ -16,6 +16,33 @@ document.addEventListener('DOMContentLoaded', () => {
     let rightTimerId
     let score = 0
 
+
+    /*
+        Création de platform.
+        Une boucle qui va créer des plateforms selon le nombre de platFormCount.
+            On crée une variable platGap qui aura comme valeur, la taille de grid / par le nb de plateforme
+            On crée une variable newPlatBottom qui aura comme valeur, 100 + i * (taille de grid / par le nb de plateforme). Les plateform auront leur hauteur
+            On crée la plateform qui sera un objet de class, l'argument 'newPlatBottom' est le constructor de la class
+            On ajoute cette objet dans un tableau 
+    */
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++){
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform(newPlatBottom)
+            platforms.push(newPlatform)
+            //console.log(platforms)
+        }
+    }
+
+
+    /*
+        création du doodler.
+        On ajoute dans la div 'grid', une div 'doodler' qui à été crée au lancemen du fichier.
+        On donne a cette div, un attribut class qui a pour valeur 'doodler'.
+        doodlerLeftSpace prendra la valeur de platforms[0].left qui == la valeur de la coordonnée gauche du DOMRECT (l'emplacement de la platform)
+        On donne à la div doodler, des style.left et style.bottom en px. Style.bottom aura la valeur donné au début.  
+    */
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
@@ -40,16 +67,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    function createPlatforms() {
-        for (let i = 0; i < platformCount; i++){
-            let platGap = 600 / platformCount
-            let newPlatBottom = 100 + i * platGap
-            let newPlatform = new Platform(newPlatBottom)
-            platforms.push(newPlatform)
-            console.log(platforms)
-        }
-    }
+    /*
 
+    */
     function movePlatforms() {
         if (doodlerBottomSpace > 200) {
             platforms.forEach(platform => {
